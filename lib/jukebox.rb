2ad7069e3b02eee7c2de36e7370f9632858bbb2c
Christina Sohn <chsohn15@gmail.com>
2020-06-24 03:15:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
       puts "Playing #{song}"
       break
     elsif response == song
-      puts "Playing #{songs[index -8]}"
+      puts "Playing #{songs[index -9]}"
       break
     end
   end
