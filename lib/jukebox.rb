3d8cc7aefd8c908e95b94a0453c6c45f3b96a929
Christina Sohn <chsohn15@gmail.com>
2020-06-24 03:31:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,6 +36,7 @@ response = response.chomp
       puts "Playing #{response.to_s}"
       break
     end
-  puts "Invalid input, please try again"
   end
+  puts "Invalid input, please try again"
 end
+
