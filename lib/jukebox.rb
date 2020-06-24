080e8fd99bdee5b8f7dbb72c9b3584b5fff2151f
Christina Sohn <chsohn15@gmail.com>
2020-06-24 02:24:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,5 +15,9 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts ""
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+
 end
