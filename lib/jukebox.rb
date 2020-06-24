b4af33da085ebf3164f692dee952825c19ed003a
Christina Sohn <chsohn15@gmail.com>
2020-06-24 02:35:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,6 +25,6 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   response = gets.strip
-  songs.each_with_index {|song, index| puts "Playing {song}" if response == index || response == song else puts "Invalid input, please try again"}
+  songs.each_with_index {|song, index| puts "Playing {song}" if response == index || response == song else puts "Invalid input, please try again" end}
 end
 end
