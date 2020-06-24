ef960256b4b357345e632c16e11dba1b8b268ffe
Christina Sohn <chsohn15@gmail.com>
2020-06-24 03:32:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,6 @@ end
 def play(songs)
 puts "Please enter a song name or number:"
 response = gets.strip
-response = response.chomp
   songs.each_with_index do |song, index|
     if response.to_i == index + 1 
       puts "Playing #{song}"
